eb83fff7a4c1e86dfc7843a85eb3d592ee10ab38
Heather Tovey <heather@htovey.com>
2015-07-03 00:12:17 -0600

    Add basic Frogger functionality to game.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,18 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    if (this.x > 909) {
+        this.x = 1;
+    } else {
+        this.x = this.x + (this.speed * dt);
+    }
+    this.y = this.y;
+
+
+    if (Math.abs(this.x - player.x) < 50 && Math.abs(this.y - player.y) < 43) {
+        player.reset();
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +39,54 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 202;
+    this.y = 372;
 
+
+    this.render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    this.handleInput = function(key) {
+        if (key === 'up') {
+            if (this.y > 43) {
+                this.y -= 83;
+            } else {
+                this.reset();
+            }
+        } else if (key === 'down' && this.y < 372) {
+            this.y += 83;
+        } else if (key === 'left' && this.x > 1) {
+            this.x -= 101;
+        } else if (key === 'right' && this.x < 404)
+            this.x += 101;
+    }
+
+    this.update = function() {
+        return this.x;
+
+    }
+
+    this.reset = function() {
+        this.x = 202;
+        this.y = 372;
+    }
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+for (i = 0; i < 5; i++) {
+    var rand_loc = Math.round(Math.random() * 10) * 101;
+    var rand_row = Math.ceil((Math.random() * 4)) * 83 - 20;
+    var rand_speed = Math.ceil((Math.random() * 5)) * 100;
+    allEnemies.push(new Enemy(rand_loc, rand_row, rand_speed));
+}
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
